7375ce56a7264d5757edca1a00585f020fa96c5b
Hung <Hissatsu1993@gmail.com>
2019-10-08 09:48:54 +1100

    Think i found my source control issue not using Pending Files drop down leaving my new files never tracked =/

---
+++ GravityLlamaProto2D/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// MainMenu.cs main script for managing main menu button presses and scene switching
+/// 
+/// changelog
+/// 19/9 - init
+/// </summary>
+public class MainMenu : MonoBehaviour
+{
+
+    //uses scenemanager to load scene
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    //quit game/application
+    public void QuitGame()
+    {
+        Debug.Log("QuitGame!");
+        Application.Quit();
+    }
+}
---
+++ GravityLlamaProto2D/Assets/Scripts/SpaceToPlay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SpaceToPlay : MonoBehaviour
+{
+    //void OnGUI()
+    //{
+    //    Event e = Event.current;
+    //    if (e.isKey)
+    //    {
+    //        Debug.Log("Detected key code: " + e.keyCode);
+    //    }
+    //}
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("SpaceToPlay");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
