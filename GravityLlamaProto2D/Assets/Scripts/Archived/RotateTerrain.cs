89b4b26fc02dd1114cc52aae6349176a05ef458c
William Tat <100665166@student.swin.edu.au>
2019-10-22 02:14:12 +1100

    Temporary fixes to Level 2
    - Llama now scales the terrain uphill (downhill remains janky); temporary fix until script RotateCameraZone.cs is polished.

---
+++ GravityLlamaProto2D/Assets/Scripts/Archived/RotateTerrain.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * =============================================================================
+ * 
+ * [Gravity Llama]
+ * Alpha
+ * 
+ * 
+ * Script name:
+ * RotateTerrain.cs
+ * 
+ * 
+ * Date:
+ * 19-10-2019
+ * 
+ * 
+ * Description:
+ * Rotates the terrain itself (player not affected)
+ * 
+ * 
+ * Parameters:
+ * None
+ * 
+ * 
+ * Attaches to:
+ * Any GameObject with a Collider trigger
+ * 
+ * 
+ * Dependencies:
+ * None
+ * 
+ * 
+ * Changelog:
+ * 19-10    Initial
+ * 21-10    Deprecated
+ * 
+ * =============================================================================
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTerrain : MonoBehaviour
+{
+    [Tooltip("What angle should the terrain rotate towards at this point?")]
+    public float angle = 0f;
+
+    void OnTriggerEnter(Collider col)
+    {
+        // Are we the player?
+        if (col.CompareTag("Player"))
+        {
+            transform.parent.Rotate(new Vector3(0, 0, angle));
+        }
+    }
+}
---
+++ GravityLlamaProto2D/Assets/Scripts/RotateCameraZone.cs
@@ -0,0 +1,100 @@
+﻿/*
+ * =============================================================================
+ * 
+ * [Gravity Llama]
+ * Alpha
+ * 
+ * 
+ * Script name:
+ * RotateCameraZone.cs
+ * 
+ * 
+ * Date:
+ * 19-10-2019
+ * 
+ * 
+ * Description:
+ * Rotates the player's camera to match incline of the level terrain
+ * 
+ * 
+ * Parameters:
+ * None
+ * 
+ * 
+ * Attaches to:
+ * Any GameObject with a Collider trigger
+ * 
+ * 
+ * Dependencies:
+ * None
+ * 
+ * 
+ * Changelog:
+ * 21-10    Initial
+ * 
+ * =============================================================================
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCameraZone : MonoBehaviour
+{
+    [Tooltip("What angle should the camera rotate to?")]
+    public float angle = 0f;
+
+    [Tooltip("How much does the player's jump get boosted? (to accomodate for height slopes)")]
+    public float playerNewJumpStrength = 5000f;
+
+    [Tooltip("Will the player's weight increase dramatically at this zone? (to compensate for steep inclines)")]
+    public bool heavyZone = false;
+
+    // For the MainCamera
+    private GameObject cam;
+
+    // ********************************************************************************************************
+
+    void Start()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    // ********************************************************************************************************
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawSphere(transform.position, 25f);
+    }
+
+    // ========================================================================================================
+    // ********************************************************************************************************
+    // ========================================================================================================
+    // ********************************************************************************************************
+    // ========================================================================================================
+    // ********************************************************************************************************
+    // ========================================================================================================
+    // ********************************************************************************************************
+    // ========================================================================================================
+    // ********************************************************************************************************
+    // ========================================================================================================
+
+    void OnTriggerEnter(Collider col)
+    {
+        // Are we the player?
+        if (col.CompareTag("Player"))
+        {
+            //cam.transform.Rotate(new Vector3(angle, 0, 0));
+            cam.transform.Rotate(new Vector3(angle, 0, 0));
+            col.GetComponent<Player>().SetJumpStrength = playerNewJumpStrength;
+
+            // Don't make us drop to the ground if we're jumping
+            if (heavyZone && !col.GetComponent<Player>().IsJumping)
+            {
+                // Make them fall to the ground quickly
+                col.GetComponent<Rigidbody>().AddForce((transform.position - col.transform.position) * 20000f * Time.smoothDeltaTime);
+            }
+        }
+    }
+}
