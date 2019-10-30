895553cf6e48f4ae6269f5a3dddbfd18e21b4499
William Tat <100665166@student.swin.edu.au>
2019-10-30 13:24:57 +1100

    Camera smoothing re-added
    - Was missing for some reason. Added back into script.
    - Can be adjusted in the trigger box. Can set how long camera takes to smooth and what angle it rotates towards (Negative = up/Postive = down).

--- GravityLlamaProto2D/Assets/Scripts/RotateCameraZone.cs
+++ GravityLlamaProto2D/Assets/Scripts/RotateCameraZone.cs
@@ -43,9 +43,12 @@ using UnityEngine;
 
 public class RotateCameraZone : MonoBehaviour
 {
-    [Tooltip("What angle should the camera rotate to?")]
+    [Tooltip("What angle should the camera rotate to?\n\nNOTE:\nNegative angles = camera swivels up\nPositive angles = camera swivels down")]
     [Range(-12, 12)]    // Should never be more than 12...
     public float angle = 0f;
+
+    [Tooltip("How long should the camera take to smooth itself? (shouldn't be higher than 1.0 or less than 0.5 for best results)")]
+    public float cameraTransitionTime = 1f;
 
     [Tooltip("How much does the player's jump get boosted? (to accomodate for height slopes)")]
     public float playerNewJumpStrength = 5000f;
@@ -86,8 +89,28 @@ public class RotateCameraZone : MonoBehaviour
         if (col.CompareTag("Player"))
         {
             //cam.transform.Rotate(new Vector3(angle, 0, 0));
-            cam.transform.Rotate(new Vector3(angle, 0, 0));
+            StartCoroutine(CameraTransition(Vector3.right * angle, cameraTransitionTime));
             col.GetComponent<Player>().SetJumpStrength = playerNewJumpStrength;
+        }
+    }
+
+    // ********************************************************************************************************
+
+    // CameraTransition
+    // Smoothly rotates the camera to angle specified by trigger box
+    // Takes: Vector3, float
+    // Returns: Nothing
+    IEnumerator CameraTransition(Vector3 angle, float duration)
+    {
+        var fromAngle = cam.transform.rotation;
+        var toAngle = Quaternion.Euler(cam.transform.eulerAngles + angle);
+        for (var t = 0f; t < 1; t += Time.deltaTime / duration)
+        {
+            // TODO: Something better perhaps?
+            cam.transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+
+            // Stop rotating once we reach angle/time limit
+            yield return null;
         }
     }
 }
